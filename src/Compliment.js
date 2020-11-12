0e22bec449fb495c80718809c4b91ca912e16bb0
Austin Benckendorf <BenckendorfAustin@johndeere.com>
2020-11-12 13:38:49 -0600

    feature 2 changes

---
+++ src/Compliment.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+export default function Compliment(props) {
+    return (
+        <p>Hello {props.data[23]}</p>)
+}
+
+// [Math.floor(Math.random() * data.length)]
